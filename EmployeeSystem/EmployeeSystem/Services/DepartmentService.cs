ed8e60346579b7b7b88f1f553091a1471d027cc9
unV1RUS <danipate091@gmail.com>
2025-09-18 15:40:39 +0300

    add services

---
+++ EmployeeSystem/EmployeeSystem/Services/DepartmentService.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeSystem.Data;
+using EmployeeSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystem.Services
+{
+    public class DepartmentService
+    {
+        private EmployeeSystemContext _context = new();
+
+        public async Task<List<Department>> GetDepartmentsAsync()
+        {
+            return await _context.Departments.Include(d=>d.Employees).ToListAsync();
+        }
+
+        public void DeleteDepartment(Department department)
+        {
+            _context.Departments.Remove(department);
+        }
+
+        public void UpdateDepartment(Department changedDepartment)
+        {
+            _context.Departments.Update(changedDepartment);
+        }
+
+        public async Task AddDepartmentsAsync(Department newDepartment)
+        {
+            await _context.Departments.AddAsync(newDepartment);
+        }
+    }
+}
---
+++ EmployeeSystem/EmployeeSystem/Services/EmployeeService.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeSystem.Data;
+using EmployeeSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystem.Services
+{
+    public class EmployeeService
+    {
+        private EmployeeSystemContext _context = new();
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            return await _context.Employees.Include(e=>e.Department).ToListAsync();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            _context.Employees.Remove(employee);
+        }
+
+        public void UpdateEmployee(Employee changedEmployee)
+        {
+            _context.Employees.Update(changedEmployee); 
+        }
+
+        public async Task AddEmployeesAsync(Employee newEmployee)
+        {
+            await _context.Employees.AddAsync(newEmployee);
+        }
+    }
+}
